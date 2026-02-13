fc88b391288aaa88445c3288bbfafbe934f51b85
Bhone <bhonemyatoo73@gmail.com>
2026-02-13 12:29:43 +0700

    Modify footer with new address and contact link
    Updated address and added a contact request link in footer.

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -65,7 +65,7 @@
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                 d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
             </svg>
-            <span>123 University Drive, London, United Kingdom 12345</span>
+            <span>Manchester, United Kingdom 12345</span>
           </li>
           <li class="flex items-center space-x-2">
             <svg class="w-5 h-5 text-[#dc2d3d] flex-shrink-0" fill="none" stroke="currentColor" viewBox="0 0 24 24">
@@ -80,6 +80,16 @@
                 d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
             </svg>
             <span>+44 (555) 123-4567</span>
+          </li>
+          <li>
+            <a href="{{ route('contact.create') }}"
+              class="inline-flex items-center space-x-2 bg-[#dc2d3d] hover:bg-[#b82532] hover:text-white text-white text-sm dark:text-white dark:hover:text-white font-medium px-12 py-3 rounded-lg transition-colors">
+              <svg class="w-5 h-5 flex-shrink-0" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                  d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z" />
+              </svg>
+              <span>Send Contact Request</span>
+            </a>
           </li>
         </ul>
       </div>

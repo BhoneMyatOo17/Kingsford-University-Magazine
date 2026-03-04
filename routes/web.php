d9ce9f403f88c6b4735dd30450d7fd750c4d8ad6
BhoneMyatOo17 <bhonemyatoo73@gmail.com>
2026-03-04 13:43:30 +0700

    Academic Year, Posts, and Contributions

--- routes/web.php
+++ routes/web.php
@@ -135,11 +135,6 @@ Route::middleware(['auth'])->group(function () {
     Route::middleware(['role:admin'])->group(function () {
         Route::post('posts/{id}/restore', [PostController::class, 'restore'])->name('posts.restore');
         Route::resource('posts', PostController::class);
-        Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
-        Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
-        Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
-        Route::put('/posts/{post}', [PostController::class, 'update'])->name('posts.update');
-        Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
         Route::resource('academic-years', AcademicYearController::class);
     });
 
